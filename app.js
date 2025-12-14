a79cde67303e70081264eb7111b7136f9eb50d21
kaleshwar109 <kaleshwar.yadav@gspann.com>
2025-12-15 01:59:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,2 @@
-function add(a, b) {
-  return a + b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-function calculate(operation, a, b) {
-  if (operation == "add") {
-    return add(a, b);
-  }
-  if (operation == "divide") {
-    return divide(a, b);
-  }
-}
-
-console.log(calculate("add", "5", 10));
-console.log(calculate("divide", 10, 0));
+console.log("its for testing");
 
